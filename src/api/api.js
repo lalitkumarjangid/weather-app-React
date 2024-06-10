31b2b6188ed73e585ac08f63e2d26a19fcd28dcc
Lalit Kumar Jangid <lkjlalitkumar2005@gmail.com>
2024-06-10 14:08:46 +0530

    first commit

---
+++ src/api/api.js
@@ -0,0 +1,11 @@
+const apiKey = 'db92bafa40e1a06b776c9f41d000b1d9';
+
+const getWeather = async (city) => {
+    return await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`)
+    .then((res) => res.json())
+    .then((json) => {
+        return json;
+    })
+}
+
+export default getWeather;
